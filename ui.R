340a1db544f10141d309e18cbaff5a24a90ea2a4
lil-ds <lilyshen@uw.edu>
2022-06-02 01:35:38 -0700

    changes to paragraph er

--- ui.R
+++ ui.R
@@ -91,7 +91,7 @@ electric_range_tab <- tabPanel(
     er_main_panel_plot,
   ),
   p("This dot plot maps out",
-    span("electric range (how far a vehicle can travel purely on electric charge)", style = "color:blue"),
+    span("electric range (amount of miles a vehicle can travel purely on electric charge)", style = "color:blue"),
     "of electric vehicles from 1993-2022, organized by different makes to observe efficiency!"),
   p("The scatterplot is also organized by vehicle models, allowing observations of specific models by makers.")
 )
